09a7336912688c3d9c7078047030da28dc6ea594
Ronyszu <ronyszu@gmail.com>
2020-07-04 19:04:32 -0300

    Add project files.

---
+++ MqlSql/MqlSql.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using RGiesecke.DllExport;
+
+namespace MqlSql
+{
+    public static class MqlSql
+    {
+        private static SqlConnection conn = null;
+        private static SqlCommand com = null;
+        private static string sMessage = string.Empty;
+        public const int iResSuccess = 0;
+        public const int iResError = 1;
+
+
+        [DllExport("CreateConnection", CallingConvention = CallingConvention.StdCall)]
+        public static int CreateConnection(
+        [MarshalAs(UnmanagedType.LPWStr)] string sConnStr)
+        {
+            // Limpamos a linha de mensagem:
+            sMessage = string.Empty;
+            // Se já houver conexão, fechamos e mudamos
+            // cadeia de conexão para uma nova, se não -
+            // recriamos os objetos de conexão e os comandos:
+            if (conn != null)
+            {
+                conn.Close();
+                conn.ConnectionString = sConnStr;
+            }
+            else
+            {
+                conn = new SqlConnection(sConnStr);
+                com = new SqlCommand();
+                com.Connection = conn;
+            }
+            // Tentamos abrir uma conexão:
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                // Por algum motivo, a conexão não foi aberta.
+                // Colocamos as informações de erro na linha de mensagens:
+                sMessage = ex.Message;
+                // Liberamos recursos e redefinimos objetos:
+                com.Dispose();
+                conn.Dispose();
+                conn = null;
+                com = null;
+                // Erro:
+                return iResError;
+            }
+            // Tudo correu bem, a conexão está aberta:
+            return iResSuccess;
+        }
+
+
+        [DllExport("GetLastMessage", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string GetLastMessage()
+        {
+            return sMessage;
+        }
+
+        [DllExport("ExecuteSql", CallingConvention = CallingConvention.StdCall)]
+        public static int ExecuteSql(
+        [MarshalAs(UnmanagedType.LPWStr)] string sSql)
+        {
+            // Limpamos a linha de mensagem:
+            sMessage = string.Empty;
+            // Primeiro precisamos verificar se a conexão está estabelecida.
+            if (conn == null)
+            {
+                // A conexão ainda não está aberta.
+                // Notificamos sobre o erro e retornamos o sinalizador do erro:
+                sMessage = "Connection is null, call CreateConnection first.";
+                return iResError;
+            }
+            // A conexão está pronta, tentamos executar o comando.
+            try
+            {
+                com.CommandText = sSql;
+                com.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                // Erro ao executar o comando.
+                // Colocamos as informações de erro na linha de mensagens:
+                sMessage = ex.Message;
+                // Retornamos o sinalizador de erro:
+                return iResError;
+            }
+            // Tudo correu bem, portanto, devolvemos o sinalizador de execução bem-sucedida:
+            return iResSuccess;
+        }
+
+
+
+        [DllExport("ReadInt", CallingConvention = CallingConvention.StdCall)]
+        public static int ReadInt(
+        [MarshalAs(UnmanagedType.LPWStr)] string sSql)
+        {
+            // Limpamos a linha de mensagem:
+            sMessage = string.Empty;
+            // Primeiro precisamos verificar se a conexão está estabelecida.
+            if (conn == null)
+            {
+                // A conexão ainda não está aberta.
+                // Notificamos sobre o erro e retornamos o sinalizador do erro:
+                sMessage = "Connection is null, call CreateConnection first.";
+                return iResError;
+            }
+            // Variável para obter o resultado de retorno:
+            int iResult = 0;
+            // A conexão está pronta, tentamos executar o comando.
+            try
+            {
+                com.CommandText = sSql;
+                iResult = (int)com.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                // Erro ao executar o comando.
+                // Colocamos as informações de erro na linha de mensagens:
+                sMessage = ex.Message;
+            }
+            // Retornamos o resultado:
+            return iResult;
+        }
+
+
+        [DllExport("ReadString", CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        public static string ReadString(
+        [MarshalAs(UnmanagedType.LPWStr)] string sSql)
+        {
+            // Limpamos a linha de mensagem:
+            sMessage = string.Empty;
+            // Primeiro precisamos verificar se a conexão está estabelecida.
+            if (conn == null)
+            {
+                // A conexão ainda não está aberta.
+                // Notificamos sobre o erro e retornamos o sinalizador do erro:
+                sMessage = "Connection is null, call CreateConnection first.";
+                return string.Empty;
+            }
+            // Variável para obter o resultado de retorno:
+            string sResult = string.Empty;
+            // A conexão está pronta, tentamos executar o comando.
+            try
+            {
+                com.CommandText = sSql;
+                sResult = com.ExecuteScalar().ToString();
+            }
+            catch (Exception ex)
+            {
+                // Erro ao executar o comando.
+                // Colocamos as informações de erro na linha de mensagens:
+                sMessage = ex.Message;
+            }
+            // Retornamos o resultado:
+            return sResult;
+        }
+
+
+        [DllExport("CloseConnection", CallingConvention = CallingConvention.StdCall)]
+        public static void CloseConnection()
+        {
+            // Primeiro precisamos verificar se a conexão está estabelecida.
+            if (conn == null)
+                // A conexão ainda não está aberta - significa que também não precisamos fechá-la:
+                return;
+            // Conexão está aberta - devemos fechá-la:
+            com.Dispose();
+            com = null;
+            conn.Close();
+            conn.Dispose();
+            conn = null;
+        }
+
+    }
+}
---
+++ StrategyTester/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Threading;
+using System.Windows.Input;
+
+namespace StrategyTester
+{
+    public static class Program
+    {
+
+        #region Nested classes to support running as service
+        public const string ServiceName = "StrategyTesterService";
+
+        public class Service : ServiceBase
+        {
+            public Service()
+            {
+                ServiceName = Program.ServiceName;
+            }
+
+            protected override void OnStart(string[] args)
+            {
+                Program.Start(args);
+            }
+
+            protected override void OnStop()
+            {
+                Program.Stop();
+            }
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Starting Stategy Tester");
+            Console.WriteLine("Developed by Rony Szuster");
+
+
+            if (!Environment.UserInteractive)
+                // running as service
+                using (var service = new Service())
+                    ServiceBase.Run(service);
+            else
+            {
+                // running as console app
+                Start(args);
+
+                Console.WriteLine("Press any key to stop...");
+                Console.ReadKey(true);
+
+                Stop();
+            }
+        }
+
+        private static void Start(string[] args)
+        {
+
+            Console.WriteLine("Trying to open strategy file");
+
+            //Perform desired changes in the file
+            string text = File.ReadAllText(@"C:\Users\ronys\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Experts\DojiStarRígido.mq5");
+            text = text.Replace("0.03", "0.05");
+            File.WriteAllText(@"C:\Users\ronys\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Experts\DojiStarRígido.mq5", text);
+
+            //Run MQL5 editor
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"C:\Users\ronys\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Experts\DojiStarRígido.mq5")
+            {
+               UseShellExecute = true
+            };
+            p.Start();
+
+            //wait for EA to open up  TODO: improve this
+            System.Threading.Thread.Sleep(12000);
+
+            if (p.Responding){
+                //Run Expert Advisor (manually) -> simulate keys CTRL + F5
+
+                KeyboardInput.SendCTRLF5();
+
+            }
+
+
+            Console.WriteLine("Ran strategy file");
+        }
+
+        private static void Stop()
+        {
+            // onstop code here
+        }
+
+
+
+
+    }
+}
